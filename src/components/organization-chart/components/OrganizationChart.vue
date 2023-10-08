ef0541aeb72160e376dea90ccc48f6b1f2bd8004
BryanS1989 <bryanstevenal@gmail.com>
2023-10-08 19:49:41 +0200

    Create drag element management

--- src/components/organization-chart/components/OrganizationChart.vue
+++ src/components/organization-chart/components/OrganizationChart.vue
@@ -14,13 +14,73 @@ export default {
     },
     data() {
         return {
+            clicked: false,
+            position: {
+                start: {
+                    x:0,
+                    y:0
+                },
+                end: {
+                    x: 0,
+                    y: 0
+                },
+                offset: {
+                    top: 0,
+                    left: 0
+                }
+            }
         }
+    },
+    computed:{
+        top () {
+            return `${this.position.offset.top}px`;
+        },
+        left() {
+            return `${this.position.offset.left}px`;
+        },
+        positionAbsolute () {
+            return {
+                top: this.top,
+                left: this.left
+            }
+        }
+    },
+    methods: {
+        startDrag(mouseEvent) {
+            const { clientX, clientY } = mouseEvent;
+            this.clicked = true;
+            this.position.start = {x: clientX, y: clientY };
+        },
+        dragChart(mouseEvent) {
+            const { clientX, clientY } = mouseEvent;
+            const { offsetTop, offsetLeft } = this.$refs['org-chart'];
+
+            if (this.clicked) {
+                this.position.end = { x: this.position.start.x - clientX, y: this.position.start.y - clientY };
+                this.position.start = { x: clientX, y: clientY };
+                this.position.offset = { top: offsetTop - this.position.end.y, left: offsetLeft - this.position.end.x };
+            }
+        },
+        endDrag(mouseEvent) {
+            this.clicked = false;
+        },
     }
 }
 </script>
 
 <template>
-    <section class="w-full p-2">
-        <OrganizationTree :levelInfo="chartInfo"/>
-    </section>
+    <article 
+        class="relative border border-neutral-300 rounded-xl overflow-hidden w-full h-full p-6 flex justify-around items-center bg-neutral-50 shadow-inner" 
+        @mousedown="startDrag" 
+        @mousemove="dragChart"
+        @mouseup="endDrag"
+    >
+        <section 
+            ref="org-chart" 
+            class="absolute w-full h-full flex-1 flex justify-around items-center cursor-grab" 
+            :style="positionAbsolute"
+        >
+            <OrganizationTree :levelInfo="chartInfo"/>
+        </section>
+    </article>
 </template>
