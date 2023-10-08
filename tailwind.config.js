47112b1b8420752447b9c5a46b0acd6f7be805a9
BryanS1989 <bryanstevenal@gmail.com>
2023-10-08 18:43:40 +0200

    Fix tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [
+  content: [
     './index.html', 
     './src/**/*.{vue,js,ts,jsx,tsx}'
   ],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
