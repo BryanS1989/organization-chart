675d15c9894715ac841497fbe2639b03ce0ac64e
BryanS1989 <bryanstevenal@gmail.com>
2023-10-08 19:07:03 +0200

    Add classes to app

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,11 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header class="flex flex-col justify-around items-start gap-2 py-4">
-    <h1 class="text-4xl">Organization Chart</h1>
-    <nav class="flex justify-around items-center gap-2 text-xl">
+  <header class="p-8 bg-neutral-50 flex justify-between items-center">
+    <h1 class="text-4xl font-bold">Organization Chart</h1>
+    <nav class="flex justify-start items-center gap-2 text-xl">
         <RouterLink to="/">Demo</RouterLink>
-      </nav>
+    </nav>
   </header>
 
   <RouterView />
