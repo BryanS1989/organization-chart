31c7a48a74a6e40f58572650a28c3dbfa57ede7e
BryanS1989 <bryanstevenal@gmail.com>
2023-10-12 22:05:50 +0200

    change employeeInfo to nodeInfo and add hasChildren property

--- src/components/organization-chart/components/OrganizationNode.vue
+++ src/components/organization-chart/components/OrganizationNode.vue
@@ -5,10 +5,14 @@ export default {
     components: {
     },
     props: {
-        employeeInfo: {
+        nodeInfo: {
             type: Object,
             required: true
         },
+        hasChildren:{
+            type: Boolean,
+            default: false
+        }
     },
     data() {
         return {
@@ -17,10 +21,7 @@ export default {
     },
     computed: {
         properties () {
-            return Object.keys(this.employeeInfo).filter((key) => !Array.isArray(this.employeeInfo[key]) && !['name', 'position'].includes(key));
-        },
-        subordinates() {
-            return this.employeeInfo.employees ?? false;
+            return Object.keys(this.nodeInfo).filter((key) => !Array.isArray(this.nodeInfo[key]) && !['name', 'position'].includes(key));
         },
     },
     methods: {
@@ -35,21 +36,21 @@ export default {
 <template>
     <section class="relative border border-blue-700 rounded-2xl w-80 select-none shadow-lg min-h-40 flex flex-col flex-wrap justify-between items-stretch">
         <header class="border-b px-4 py-2 bg-blue-700 rounded-t-xl text-neutral-100">
-            <h3 class="text-xl font-bold whitespace-nowrap">{{ employeeInfo.name }}</h3>
-            <p class="text-xs text-thin text-left" v-show="employeeInfo.position">{{ employeeInfo.position }}</p>
+            <h3 class="text-xl font-bold whitespace-nowrap">{{ nodeInfo.name }}</h3>
+            <p class="text-xs text-thin text-left" v-show="nodeInfo.position">{{ nodeInfo.position }}</p>
         </header>
 
         <ul class="grow p-4 flex flex-col justify-start items-start">
             <li v-for="property in properties" :key="property">
-                <span class="font-semibold">{{ property }}: </span><span class="text-xs">{{ this.employeeInfo[property] }}</span>
+                <span class="font-semibold">{{ property }}: </span><span class="text-xs">{{ this.nodeInfo[property] }}</span>
             </li>
         </ul>
 
         <footer 
-            :class="`h-5 relative flex justify-center items-center ${subordinates ? 'cursor-pointer' : ''}`"
+            :class="`h-5 relative flex justify-center items-center ${hasChildren ? 'cursor-pointer' : ''}`"
             @click="emitExpand"
         >
-            <div v-if="subordinates" 
+            <div v-if="hasChildren" 
                 class="absolute -bottom-4 z-10 bg-neutral-100 border border-blue-700 rounded-full w-8 h-8 flex justify-center items-center text-blue-700" 
             >
                     <font-awesome-icon v-if="!expanded" :icon="['fas', 'chevron-down']" />
