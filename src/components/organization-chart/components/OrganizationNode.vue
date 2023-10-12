320e22510df4258d4cb68c9df6f4528367e3d84c
BryanS1989 <bryanstevenal@gmail.com>
2023-10-12 22:46:33 +0200

    Add querySelector prop, create isHighlighted to mark node if node is a search result

--- src/components/organization-chart/components/OrganizationNode.vue
+++ src/components/organization-chart/components/OrganizationNode.vue
@@ -12,17 +12,49 @@ export default {
         hasChildren:{
             type: Boolean,
             default: false
+        },
+        querySelector: {
+            type: Object,
+            default: () => ({})
         }
     },
     data() {
         return {
-            expanded: true
+            expanded: true,
+            isHighlighted: false
         }
     },
     computed: {
         properties () {
             return Object.keys(this.nodeInfo).filter((key) => !Array.isArray(this.nodeInfo[key]) && !['name', 'position'].includes(key));
         },
+        highlightedSectionClass () {
+            return !this.isHighlighted ? 'border-blue-700' : 'border-amber-500'
+        },
+        highlightedHeaderClass() {
+            return !this.isHighlighted ? 'bg-blue-700' : 'bg-amber-500'
+        }
+    },
+    watch: {
+        querySelector: {
+            deep: true,
+            handler() {
+                Object.keys(this.querySelector).map((property) => {
+                    switch (typeof this.querySelector[property]) {
+                        case 'string':
+                            if (this.querySelector[property].trim().length) {
+                                this.isHighlighted = this.nodeInfo[property]?.toLowerCase().includes(this.querySelector[property].toLowerCase()) ?? false;
+                            } else {
+                                this.isHighlighted = false;
+                            }
+                            break;
+                    
+                        default:
+                            break;
+                    }
+                });
+            }
+        }
     },
     methods: {
         emitExpand(){
@@ -34,8 +66,8 @@ export default {
 </script>
 
 <template>
-    <section class="relative border border-blue-700 rounded-2xl w-80 select-none shadow-lg min-h-40 flex flex-col flex-wrap justify-between items-stretch">
-        <header class="border-b px-4 py-2 bg-blue-700 rounded-t-xl text-neutral-100">
+    <section :class="`relative border ${highlightedSectionClass} rounded-2xl w-80 select-none shadow-lg min-h-40 flex flex-col flex-wrap justify-between items-stretch`">
+        <header :class="`border-b px-4 py-2 ${highlightedHeaderClass} rounded-t-xl text-neutral-100`">
             <h3 class="text-xl font-bold whitespace-nowrap">{{ nodeInfo.name }}</h3>
             <p class="text-xs text-thin text-left" v-show="nodeInfo.position">{{ nodeInfo.position }}</p>
         </header>
