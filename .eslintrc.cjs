b8f9dab4168afe0bad28e2f7512f5531ec3ca816
BryanS1989 <bryanstevenal@gmail.com>
2023-10-14 13:48:21 +0200

    change eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,19 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
-  root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    root: true,
+    env: {
+        browser: true,
+        node: true,
+    },
+    extends: ['plugin:vue/vue3-essential', 'eslint:recommended', 'prettier'],
+    rules: {
+        'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    },
+    parserOptions: {
+        ecmaVersion: 'latest',
+    },
+};
