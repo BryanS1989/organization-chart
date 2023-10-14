315de57f6bc1255f2fb8164037e50eea9f8aa33a
BryanS1989 <bryanstevenal@gmail.com>
2023-10-14 13:03:36 +0200

    Use AppLink

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
   <header class="px-8 py-4 bg-blue-700 flex justify-between items-center">
     <h1 class="text-4xl font-bold text-neutral-100">Organization Chart</h1>
-    <nav class="flex justify-start items-center gap-2 text-xl">
-        <RouterLink to="/">Demo</RouterLink>
+    <nav class="flex justify-start items-center gap-12 text-xl">
+      <AppLink to="/">Demo</AppLink>
+      <AppLink to="https://github.com/BryanS1989">BryanS1989</AppLink>
     </nav>
   </header>
 
